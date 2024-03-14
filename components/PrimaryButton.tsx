063e39728fdc308b591fc53358a9a12ffc9ec217
hchang604 <henry.chang@alianza.com>
2024-03-13 22:50:53 -0700

    convert files to typescript and implement initial styling

---
+++ components/PrimaryButton.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {View, Text, Pressable, StyleSheet} from 'react-native';
+
+type PrimaryButtonProps = {
+  textValue: string;
+};
+
+function PrimaryButton(props: PrimaryButtonProps) {
+  function pressHandler() {
+    console.log('pressed');
+  }
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        /* We try to achieve the same effect of android_ripple on iOS here */
+        style={({pressed}) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{color: '#640233'}}>
+        <Text style={styles.buttonText}>{props.textValue}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    /* Android specific styling */
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
+
+export default PrimaryButton;
