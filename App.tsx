70052ff8dfd153b9e1a561884f46cebcee843e18
hchang604 <henry.chang@alianza.com>
2024-03-14 21:56:18 -0700

    add app background image

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import StartGameScreen from './screens/StartGameScreen';
-import {StyleSheet} from 'react-native';
+import {ImageBackground, StyleSheet} from 'react-native';
 import {LinearGradient} from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}>
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -14,6 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: '#ddb52f',
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
