d27adbd5670dc13a89ce45affb6214eb6a17b994
hchang604 <henry.chang@alianza.com>
2024-03-17 22:34:51 -0700

    implement device dimension compatibility

--- App.tsx
+++ App.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import StartGameScreen from './screens/StartGameScreen';
-import {ImageBackground, StyleSheet} from 'react-native';
+import {ImageBackground, SafeAreaView, StyleSheet} from 'react-native';
 import {LinearGradient} from 'expo-linear-gradient';
+import GameScreen from './screens/GameScreen';
+import Colors from './constants/colors';
 
 export default function App() {
+  /* The number you want the computer to guess in the game */
+  const [userNumber, setUserNumber] = useState<number | undefined>();
+
+  function confirmNumberHandler(pickedNumber: number) {
+    setUserNumber(pickedNumber);
+  }
+
+  let screen = <StartGameScreen onConfirmNumber={confirmNumberHandler} />;
+
+  if (userNumber) {
+    screen = <GameScreen userNumber={userNumber} />;
+  }
+
   return (
-    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+    <LinearGradient
+      colors={[Colors.primary700, Colors.accent500]}
+      style={styles.rootScreen}>
       <ImageBackground
         source={require('./assets/images/background.png')}
         resizeMode="cover"
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}>
-        <StartGameScreen />
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
